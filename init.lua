0939d18ab9e2f63cbe1d4ae86fac3f3766624382
kksat <22549266+kksat@users.noreply.github.com>
2024-02-10 22:22:36 +0100

    add font configuration for goneovim
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -4,6 +4,11 @@ require("config.lazy")
 if vim.g.neovide then
 	vim.o.guifont = "CaskaydiaCove Nerd Font:h18"
 end
+
 if vim.g.gui_vimr then
+	vim.o.guifont = "CaskaydiaCove Nerd Font:h18"
+end
+
+if vim.g.goneovim then
 	vim.o.guifont = "CaskaydiaCove Nerd Font:h18"
 end
