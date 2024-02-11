d8b7652c45b66450b700f662bc0f6353fb12c1b3
kksat <22549266+kksat@users.noreply.github.com>
2024-02-11 01:10:07 +0100

    change configuration for chatgpt
    most important configuration - max number of tokens that returned by model
    
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -3,7 +3,24 @@ return {
 		"jackMort/ChatGPT.nvim",
 		event = "VeryLazy",
 		config = function()
-			require("chatgpt").setup()
+			require("chatgpt").setup({
+				openai_params = {
+					model = "gpt-3.5-turbo",
+					frequency_penalty = 0,
+					presence_penalty = 0,
+					max_tokens = 4096,
+					temperature = 0,
+					top_p = 1,
+					n = 1,
+				},
+				popup_layout = {
+					default = "center",
+					center = {
+						width = "100%",
+						height = "100%",
+					},
+				},
+			})
 		end,
 		dependencies = {
 			"MunifTanjim/nui.nvim",
