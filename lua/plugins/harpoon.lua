8bce4a932965bf2c71e627d07d312659b1f694ce
kksat <22549266+kksat@users.noreply.github.com>
2024-02-12 22:37:50 +0100

    change harpoon ui to use telescope
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,13 +6,30 @@ return {
 		local harpoon = require("harpoon")
 		---@diagnostic disable-next-line: missing-parameter
 		harpoon.setup()
+		local conf = require("telescope.config").values
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {}
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value)
+			end
 
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+				})
+				:find()
+		end
 		vim.keymap.set("n", "<leader>ha", function()
 			harpoon:list():append()
 		end, { desc = "Harpoon add mark" })
 
 		vim.keymap.set("n", "<leader>hl", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
+			toggle_telescope(harpoon:list())
 		end, { desc = "Harpoon list" })
 	end,
 }
