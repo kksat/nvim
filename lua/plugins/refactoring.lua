10264fd2c9f24fd288c41b83a3c1e6d1abad2ade
kksat <22549266+kksat@users.noreply.github.com>
2024-02-12 22:06:55 +0100

    add Primeagen refactoring plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

---
+++ lua/plugins/refactoring.lua
@@ -0,0 +1,20 @@
+-- SPDX-FileCopyrightText: 2024 Kirill Satarin (@kksat) <22549266+kksat@users.noreply.github.com>
+-- SPDX-License-Identifier: MIT
+
+return {
+	"ThePrimeagen/refactoring.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("refactoring").setup({
+			show_success_message = true,
+		})
+
+		vim.keymap.set("n", "<leader>cp", function()
+			---@diagnostic disable-next-line: missing-parameter
+			require("refactoring").select_refactor()
+		end, { desc = "Primeagen Refactore" })
+	end,
+}
