e28f54358d02bf77f1ada25fdf0082bf65d12739
kksat <22549266+kksat@users.noreply.github.com>
2024-02-12 14:55:30 +0100

    add sniprun plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

---
+++ lua/plugins/sniprun.lua
@@ -0,0 +1,11 @@
+return {
+	"michaelb/sniprun",
+	branch = "master",
+	--	build = "sh install.sh",
+	config = function()
+		require("sniprun").setup({
+			live_mode_toggle = "enable",
+		})
+		-- configure keymap for :SnipLive
+	end,
+}
