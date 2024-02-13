43c8f0485190c173894d5bf7e861e55dd99f5698
kksat <22549266+kksat@users.noreply.github.com>
2024-02-13 21:54:04 +0100

    add git-commit plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -17,3 +17,14 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "BufEnter" }, {
 })
 
 -- vim.g.copilot_filetypes = { "*" = true }
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "GitConflictDetected",
+	callback = function()
+		vim.notify("Conflict detected in " .. vim.fn.expand("<afile>"))
+		vim.keymap.set("n", "cww", function()
+			engage.conflict_buster()
+			create_buffer_local_mappings()
+		end)
+	end,
+})
