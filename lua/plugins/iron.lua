94b30f4ba0c47a9a5eb90d9aece4d84a437adcf4
kksat <22549266+kksat@users.noreply.github.com>
2024-02-21 10:30:09 +0100

    add iron.nvim plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

---
+++ lua/plugins/iron.lua
@@ -0,0 +1,23 @@
+return {
+	"Vigemus/iron.nvim",
+	config = function()
+		require("iron.core").setup({
+			config = {
+				scratch_repl = true,
+				-- Your repl definitions come here
+				repl_definition = {
+					sh = {
+						command = { "zsh" },
+					},
+				},
+				python = {
+					command = { "python" },
+				},
+			},
+		})
+		vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>", { desc = "Start REPL" })
+		vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>", { desc = "Restart REPL" })
+		vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>", { desc = "Focus REPL" })
+		vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>", { desc = "Hide REPL" })
+	end,
+}
