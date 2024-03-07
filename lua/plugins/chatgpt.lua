443ab658f5edc4a0d96a12e16a164d5e3e087382
kksat <22549266+kksat@users.noreply.github.com>
2024-03-07 20:00:26 +0100

    change chat gpt configuration
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -5,7 +5,7 @@ return {
 		config = function()
 			require("chatgpt").setup({
 				openai_params = {
-					model = "gpt-3.5-turbo",
+					-- model = "gpt-3.5-turbo",
 					frequency_penalty = 0,
 					presence_penalty = 0,
 					max_tokens = 4096,
