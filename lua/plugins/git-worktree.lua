e36442eda166035a1ec39084ddccc2ffa5d2410d
kksat <22549266+kksat@users.noreply.github.com>
2024-03-18 19:12:13 +0100

    refactor configuration
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/git-worktree.lua
+++ lua/plugins/git-worktree.lua
@@ -2,20 +2,20 @@ return {
 	"ThePrimeagen/git-worktree.nvim",
 	lazy = true,
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	keys = {
+		{
+			"<leader>gw",
+			"<cmd>lua require('telescope').extensions.git_worktree.git_worktree()<cr>",
+			desc = "Git Worktree",
+		},
+		{
+			"<leader>gW",
+			"<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>",
+			desc = "Create Git worktree",
+		},
+	},
 	config = function()
 		require("git-worktree").setup()
 		require("telescope").load_extension("git_worktree")
-		vim.keymap.set(
-			"n",
-			"<leader>gw",
-			"<cmd>lua require('telescope').extensions.git_worktree.git_worktree()<cr>",
-			{ desc = "Git Worktree" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>gW",
-			"<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>",
-			{ desc = "Create Git worktree" }
-		)
 	end,
 }
