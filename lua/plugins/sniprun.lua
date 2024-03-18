9516e07872c380efb5fae3952204681222bb2976
kksat <22549266+kksat@users.noreply.github.com>
2024-03-18 15:53:14 +0100

    add build
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/sniprun.lua
+++ lua/plugins/sniprun.lua
@@ -1,7 +1,7 @@
 return {
 	"michaelb/sniprun",
 	branch = "master",
-	--	build = "sh install.sh",
+  build = "sh install.sh",
 	config = function()
 		require("sniprun").setup({
 			live_mode_toggle = "enable",
