3b4eee9d997ea1bfe9b9581c9915d8319dc250f8
kksat <22549266+kksat@users.noreply.github.com>
2024-03-18 19:53:49 +0100

    refactor configuration
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -1,5 +1,24 @@
 return {
 	"akinsho/git-conflict.nvim",
 	version = "*",
-	config = true,
+	config = function()
+		require("git-conflict").setup({
+			debug = false,
+			default_mappings = true, -- disable buffer local mapping created by this plugin
+			default_commands = true, -- disable commands created by this plugin
+			disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
+			list_opener = "copen", -- command or function to open the conflicts list
+			highlights = { -- They must have background color, otherwise the default color will be used
+				incoming = "DiffAdd",
+				current = "DiffText",
+			},
+		})
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "GitConflictDetected",
+			callback = function()
+				vim.notify("Conflict detected in " .. vim.fn.expand("<afile>"))
+			end,
+		})
+	end,
 }
