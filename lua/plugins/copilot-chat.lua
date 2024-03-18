2d50ed5c8e353bb665b3254b35c94b8a30857b35
kksat <22549266+kksat@users.noreply.github.com>
2024-03-18 16:41:53 +0100

    fix github copilot chat plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -2,23 +2,18 @@ return {
 	"CopilotC-Nvim/CopilotChat.nvim",
 	branch = "canary",
 	dependencies = {
-		{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-		{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		{ "zbirenbaum/copilot.lua" },
+		{ "nvim-lua/plenary.nvim" },
 	},
 	opts = {
 		debug = false,
-		-- See Configuration section for rest
 	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {
-			require("which-key").register({
-				a = {
-					name = "ChatGPT",
-					h = { "<cmd>CopilotChatToggle<CR>", "GitHub copilot Chat" },
-				},
-			}, { prefix = "<leader>" }),
+	keys = {
+		{
+			"<leader>ah",
+			"<cmd>CopilotChatToggle<CR>",
+			mode = "n",
+			desc = "GitHub copilot Chat",
 		},
 	},
 }
