4920a7b95ddc1fdf19da7182891fb0bcfadd8452
kksat <22549266+kksat@users.noreply.github.com>
2024-03-18 22:09:41 +0100

    add configuration for ansible-playbooks
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- after/queries/python/injections.scm
+++ after/queries/python/injections.scm
@@ -4,3 +4,8 @@
 (#match? @injection.content "---")
 (#set! injection.language "yaml")
 )
+(
+((string_content) @injection.content)
+(#match? @injection.content "#!ansible-playbook")
+(#set! injection.language "yaml")
+)
