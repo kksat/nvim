80e8343b332fbad3f090569d2ffcfdea9e59de38
kksat <22549266+kksat@users.noreply.github.com>
2024-03-20 20:10:09 +0100

    change window layout
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/navbuddy.lua
+++ lua/plugins/navbuddy.lua
@@ -7,7 +7,28 @@ return {
 				"SmiteshP/nvim-navic",
 				"MunifTanjim/nui.nvim",
 			},
-			opts = { lsp = { auto_attach = true } },
+			opts = {
+				lsp = { auto_attach = true },
+				window = {
+					border = "single", -- "rounded", "double", "solid", "none"
+					size = { height = "100%", width = "100%" },
+					position = { row = "100%", col = "0%" },
+					sections = {
+						left = {
+							size = "20%",
+							border = nil,
+						},
+						mid = {
+							size = "30%",
+							border = nil,
+						},
+						right = {
+							border = nil,
+							preview = "leaf",
+						},
+					},
+				},
+			},
 			keys = {
 				{
 					"<leader>cb",
