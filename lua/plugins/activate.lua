a22c7ecd0d883f902cede636aaaddd13ef003f28
kksat <22549266+kksat@users.noreply.github.com>
2024-03-22 20:13:59 +0100

    add activate plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

---
+++ lua/plugins/activate.lua
@@ -0,0 +1,13 @@
+return {
+	"roobert/activate.nvim",
+	keys = {
+		{
+			"<leader>P",
+			'<CMD>lua require("activate").list_plugins()<CR>',
+			desc = "Plugins",
+		},
+	},
+	dependencies = {
+		{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+	},
+}
