e8c7beff4445b7f23bcdba3828d1d0ac6c621f19
kksat <22549266+kksat@users.noreply.github.com>
2024-03-24 22:40:15 +0100

    add keys for neovim-project plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -5,6 +5,18 @@ return {
 			"~/dev/*",
 		},
 	},
+	keys = {
+		{
+			"<leader>jl",
+			"<cmd>Telescope neovim-project discover<CR>",
+			desc = "Project Discover",
+		},
+		{
+			"<leader>jh",
+			"<cmd>Telescope neovim-project history<CR>",
+			desc = "Project History",
+		},
+	},
 	init = function()
 		-- enable saving the state of plugins in the session
 		vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
