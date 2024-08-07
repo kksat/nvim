366ff82d858ffece14fd80266da0dac1734f2912
kksat <22549266+kksat@users.noreply.github.com>
2024-08-07 20:56:28 +0200

    formatting
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,27 +6,28 @@
 -- Add any additional autocmds here
 --
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "BufEnter" }, {
-	group = vim.api.nvim_create_augroup("Ansible", { clear = true }),
-	pattern = {
-		"*.yml",
-		"*.yaml",
-	},
-	callback = function()
-		vim.opt.filetype = "yaml.ansible"
-	end,
+  group = vim.api.nvim_create_augroup("Ansible", { clear = true }),
+  pattern = {
+    "*.yml",
+    "*.yaml",
+  },
+  callback = function()
+    vim.opt.filetype = "yaml.ansible"
+  end,
 })
 
 -- vim.g.copilot_filetypes = { "*" = true }
 
 vim.api.nvim_create_autocmd("User", {
-	pattern = "GitConflictDetected",
-	callback = function()
-		vim.notify("Conflict detected in " .. vim.fn.expand("<afile>"))
-		vim.keymap.set("n", "cww", function()
-			engage.conflict_buster()
-			create_buffer_local_mappings()
-		end)
-	end,
+  pattern = "GitConflictDetected",
+  callback = function()
+    vim.notify("Conflict detected in " .. vim.fn.expand("<afile>"))
+    vim.keymap.set("n", "cww", function()
+      engage.conflict_buster()
+      create_buffer_local_mappings()
+    end)
+  end,
+})
 
 vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("FixBicepCommentString", { clear = true }),
