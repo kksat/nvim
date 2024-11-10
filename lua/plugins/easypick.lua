38bf4932cd583f61edc7150e8a9003f298dcbddb
kksat <22549266+kksat@users.noreply.github.com>
2024-11-10 21:54:21 +0100

    add easypick plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

---
+++ lua/plugins/easypick.lua
@@ -0,0 +1,23 @@
+return {
+  'axkirillov/easypick.nvim',
+  requires = 'nvim-telescope/telescope.nvim',
+  config = function()
+    local easypick = require("easypick")
+    local list_make_targets = [[
+    make -qp |
+    awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}' |
+    grep -wv Makefile
+    ]]
+
+    easypick.setup({
+      pickers = {
+        {
+          name = "make_targets",
+          command = list_make_targets,
+          action = easypick.actions.nvim_commandf("TermExec cmd='make %s'"),
+          opts = require('telescope.themes').get_dropdown({})
+        }
+      }
+    })
+  end
+}
