6617760d7b1937f95d0f65da96bd97e561854e81
kksat <22549266+kksat@users.noreply.github.com>
2024-12-10 17:49:24 +0100

    add keymap to search for neovim help
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,26 +1,33 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	branch = "master",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		vim.keymap.set(
-			"n",
-			"<leader>b/",
-			"<cmd>Telescope current_buffer_fuzzy_find<cr>",
-			{ desc = "Current buffer fuzzy find" }
-		)
-		require("telescope").setup({
-			defaults = {
-				mappings = {
-					n = {
-						["<c-d>"] = require("telescope.actions").delete_buffer,
-					},
-					i = {
-						["<C-h>"] = "which_key",
-						["<c-d>"] = require("telescope.actions").delete_buffer,
-					},
-				},
-			},
-		})
-	end,
+  "nvim-telescope/telescope.nvim",
+  branch = "master",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    {
+      "<leader>fh",
+      "<cmd>Telescope help_tags<cr>",
+      desc = "Telescope help_tags"
+    },
+  },
+  config = function()
+    vim.keymap.set(
+      "n",
+      "<leader>b/",
+      "<cmd>Telescope current_buffer_fuzzy_find<cr>",
+      { desc = "Current buffer fuzzy find" }
+    )
+    require("telescope").setup({
+      defaults = {
+        mappings = {
+          n = {
+            ["<c-d>"] = require("telescope.actions").delete_buffer,
+          },
+          i = {
+            ["<C-h>"] = "which_key",
+            ["<c-d>"] = require("telescope.actions").delete_buffer,
+          },
+        },
+      },
+    })
+  end,
 }
