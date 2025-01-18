5adc513d7a43d5ad106a26141737d95d6c8e861f
kksat <22549266+kksat@users.noreply.github.com>
2025-01-18 15:31:38 +0100

    do not disable arrow keys with hardtime
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,5 +1,12 @@
 return {
-   "m4xshen/hardtime.nvim",
-   dependencies = { "MunifTanjim/nui.nvim" },
-   opts = {}
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {
+    disabled_keys = {
+      ["<Up>"] = {},
+      ["<Down>"] = {},
+      ["<Left>"] = {},
+      ["<Right>"] = {},
+    },
+  }
 }
