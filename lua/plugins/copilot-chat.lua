d8a78899e3cac80dad9d17798c469f587efb7da0
kksat <22549266+kksat@users.noreply.github.com>
2025-04-30 21:39:23 +0200

    fix github copilot configuration
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,19 +1,18 @@
 return {
-	"CopilotC-Nvim/CopilotChat.nvim",
-	branch = "canary",
-	dependencies = {
-		{ "zbirenbaum/copilot.lua" },
-		{ "nvim-lua/plenary.nvim" },
-	},
-	opts = {
-		debug = false,
-	},
-	keys = {
-		{
-			"<leader>ah",
-			"<cmd>CopilotChatToggle<CR>",
-			mode = "n",
-			desc = "GitHub copilot Chat",
-		},
-	},
+  "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = {
+    { "zbirenbaum/copilot.lua" },
+    { "nvim-lua/plenary.nvim" },
+  },
+  opts = {
+    debug = false,
+  },
+  keys = {
+    {
+      "<leader>ah",
+      "<cmd>CopilotChatToggle<CR>",
+      mode = "n",
+      desc = "GitHub Copilot Chat",
+    },
+  },
 }
