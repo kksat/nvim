a8b24a190153a0094b9e591d40bb9973f0164ee3
kksat <22549266+kksat@users.noreply.github.com>
2025-05-12 22:37:21 +0200

    remove manual command to set ansible filetypes
    it is in conflict with command to set helm filetypes
    
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,18 +5,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "BufEnter" }, {
-  group = vim.api.nvim_create_augroup("Ansible", { clear = true }),
-  pattern = {
-    "*.yml",
-    "*.yaml",
-  },
-  callback = function()
-    vim.opt.filetype = "yaml.ansible"
-  end,
-})
-
--- vim.g.copilot_filetypes = { "*" = true }
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "GitConflictDetected",
