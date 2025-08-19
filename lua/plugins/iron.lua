d6a6b64b1e7a28d0039879ff82a5a4913d562005
kksat <22549266+kksat@users.noreply.github.com>
2025-08-19 21:49:55 +0200

    disable iron plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/iron.lua
+++ lua/plugins/iron.lua
@@ -1,24 +1,25 @@
 return {
-	"Vigemus/iron.nvim",
-	lazy = true,
-	config = function()
-		require("iron.core").setup({
-			config = {
-				scratch_repl = true,
-				-- Your repl definitions come here
-				repl_definition = {
-					sh = {
-						command = { "zsh" },
-					},
-				},
-				python = {
-					command = { "python" },
-				},
-			},
-		})
-		vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>", { desc = "Start REPL" })
-		vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>", { desc = "Restart REPL" })
-		vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>", { desc = "Focus REPL" })
-		vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>", { desc = "Hide REPL" })
-	end,
+  "Vigemus/iron.nvim",
+  lazy = true,
+  enabled = false,
+  config = function()
+    require("iron.core").setup({
+      config = {
+        scratch_repl = true,
+        -- Your repl definitions come here
+        repl_definition = {
+          sh = {
+            command = { "zsh" },
+          },
+        },
+        python = {
+          command = { "python" },
+        },
+      },
+    })
+    vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>", { desc = "Start REPL" })
+    vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>", { desc = "Restart REPL" })
+    vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>", { desc = "Focus REPL" })
+    vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>", { desc = "Hide REPL" })
+  end,
 }
