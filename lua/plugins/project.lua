819b4d1b0c176f3e595c9d06a3e242ed253fcac8
kksat <22549266+kksat@users.noreply.github.com>
2025-08-19 21:54:03 +0200

    disable projects plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,31 +1,32 @@
 return {
-	"coffebar/neovim-project",
-	opts = {
-		projects = { -- define project roots
-			"~/dev/*",
-		},
-	},
-	keys = {
-		{
-			"<leader>jl",
-			"<cmd>Telescope neovim-project discover<CR>",
-			desc = "Project Discover",
-		},
-		{
-			"<leader>jh",
-			"<cmd>Telescope neovim-project history<CR>",
-			desc = "Project History",
-		},
-	},
-	init = function()
-		-- enable saving the state of plugins in the session
-		vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
-	end,
-	dependencies = {
-		{ "nvim-lua/plenary.nvim" },
-		{ "nvim-telescope/telescope.nvim" },
-		{ "Shatur/neovim-session-manager" },
-	},
-	lazy = false,
-	priority = 100,
+  "coffebar/neovim-project",
+  enable = false,
+  opts = {
+    projects = { -- define project roots
+      "~/dev/*",
+    },
+  },
+  keys = {
+    {
+      "<leader>jl",
+      "<cmd>Telescope neovim-project discover<CR>",
+      desc = "Project Discover",
+    },
+    {
+      "<leader>jh",
+      "<cmd>Telescope neovim-project history<CR>",
+      desc = "Project History",
+    },
+  },
+  init = function()
+    -- enable saving the state of plugins in the session
+    vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+  end,
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim" },
+    { "Shatur/neovim-session-manager" },
+  },
+  lazy = false,
+  priority = 100,
 }
