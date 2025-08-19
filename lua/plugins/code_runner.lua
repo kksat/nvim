0e3f7e25ba172cea08f0de9cb8c4a12a8606fa8f
kksat <22549266+kksat@users.noreply.github.com>
2025-08-19 22:03:02 +0200

    disable code runner plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -1,11 +1,12 @@
 return {
-	"CRAG666/code_runner.nvim",
-	config = function()
-		require("code_runner").setup({
-			mode = "tab",
-			filetype = {
-				["yaml.ansible"] = "echo Hello",
-			},
-		})
-	end,
+  "CRAG666/code_runner.nvim",
+  enabled = false,
+  config = function()
+    require("code_runner").setup({
+      mode = "tab",
+      filetype = {
+        ["yaml.ansible"] = "echo Hello",
+      },
+    })
+  end,
 }
