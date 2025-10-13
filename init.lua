c5b7383882a1f12d7f3b1fc44d0e17068372f14d
kksat <22549266+kksat@users.noreply.github.com>
2025-10-14 00:10:52 +0200

    add keymaps for neovide
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -4,7 +4,11 @@ require("config.lazy")
 vim.opt.swapfile = false
 
 if vim.g.neovide then
-	vim.o.guifont = "CaskaydiaCove Nerd Font:h18"
+  vim.o.guifont = "Fira Code"
+
+  vim.keymap.set({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+  vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+  vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
 end
 
 if vim.g.gui_vimr then
