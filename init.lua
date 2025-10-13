9a4572f1c783f79657bb884e5d3364c1c87375b6
kksat <22549266+kksat@users.noreply.github.com>
2025-10-14 00:20:41 +0200

    remove neovide font
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -4,8 +4,6 @@ require("config.lazy")
 vim.opt.swapfile = false
 
 if vim.g.neovide then
-  vim.o.guifont = "Fira Code"
-
   vim.keymap.set({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
   vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
   vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
