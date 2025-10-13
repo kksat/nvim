d48aa5561ba3f8fd97ef921da7fd62e4243e6794
kksat <22549266+kksat@users.noreply.github.com>
2025-10-14 00:23:55 +0200

    remove unnecessary configuration
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -14,11 +14,3 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animate_in_insert_mode = true
   vim.g.neovide_cursor_animate_command_line = true
 end
-
-if vim.g.gui_vimr then
-	vim.o.guifont = "CaskaydiaCove Nerd Font:h18"
-end
-
-if vim.g.goneovim then
-	vim.o.guifont = "CaskaydiaCove Nerd Font:h18"
-end
