83338c5e0a8fb55b1f351d4edce89518ed05eee6
kksat <22549266+kksat@users.noreply.github.com>
2025-10-14 00:21:05 +0200

    add neovide configuration parameters
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -7,6 +7,12 @@ if vim.g.neovide then
   vim.keymap.set({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
   vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
   vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+
+  vim.g.neovide_cursor_animation_length = 0.100
+  vim.g.neovide_cursor_short_animation_length = 0.05
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_cursor_animate_in_insert_mode = true
+  vim.g.neovide_cursor_animate_command_line = true
 end
 
 if vim.g.gui_vimr then
