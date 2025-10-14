66b0fe55e54bb703c9f126dc8907826fe4ae8aca
kksat <22549266+kksat@users.noreply.github.com>
2025-10-14 18:24:54 +0200

    add venv selector
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,15 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+  ft = "python",
+  opts = {
+    options = {
+      name = ".venv",
+    },
+  },
+  event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  keys = {
+    { '<leader>vs', '<cmd>VenvSelect<cr>' },
+    { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+  },
+}
