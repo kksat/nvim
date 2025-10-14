7550ee801269329e0099efcfac03b067359539fc
kksat <22549266+kksat@users.noreply.github.com>
2025-10-14 08:03:39 +0200

    add session configuration
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -21,6 +21,10 @@ return {
   init = function()
     -- enable saving the state of plugins in the session
     vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+    vim.opt.sessionoptions:append("buffers")
+    vim.opt.sessionoptions:append("curdir")
+    vim.opt.sessionoptions:append("tabpages")
+    vim.opt.sessionoptions:append("terminal")
   end,
   dependencies = {
     { "nvim-lua/plenary.nvim" },
