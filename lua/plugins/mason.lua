bf059071ca1e174eba9e37d7ceb6d0e12bed6e65
kksat <22549266+kksat@users.noreply.github.com>
2025-10-14 18:29:14 +0200

    rename plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,7 +6,7 @@ return {
 		end,
 	},
 	{
-		"williamboman/mason-lspconfig.nvim",
+		"mason-org/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
