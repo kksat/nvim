63f66da70092e7fc44325d7ec690d544a6f77b96
Kirill Satarin <22549266+kksat@users.noreply.github.com>
2025-11-27 20:39:37 +0100

    do not install python-lsp-server
    Signed-off-by: Kirill Satarin <22549266+kksat@users.noreply.github.com>

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,28 +1,27 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"bashls",
-					"ansiblels",
-					"bicep",
-					"dockerls",
-					"lua_ls",
-					"marksman",
-					"python-lsp-server",
-					"yamlls",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-	},
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "bashls",
+          "ansiblels",
+          "bicep",
+          "dockerls",
+          "lua_ls",
+          "marksman",
+          "yamlls",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+  },
 }
