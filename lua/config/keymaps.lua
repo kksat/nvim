5d72257f9c6556ba1d2304f1b9ab9da89371af95
Kirill Satarin <22549266+kksat@users.noreply.github.com>
2025-12-01 15:45:54 +0100

    use snacks.nvim terminal
    Signed-off-by: Kirill Satarin <22549266+kksat@users.noreply.github.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,20 +8,12 @@ vim.keymap.set("n", "<leader>m", "<cmd>Telescope make<cr>", { desc = "Makefile" 
 
 vim.keymap.set("n", "<leader>bt", "<cmd>Telescope buffers<cr>", { desc = "Telescope list buffers" })
 
-local Terminal = require("toggleterm.terminal").Terminal
+local Terminal = require("snacks.terminal")
 
 local k9s_terminal = nil
 vim.keymap.set("n", "<leader>k", function()
   if not k9s_terminal then
-    k9s_terminal = Terminal:new({
-      cmd = "k9s",
-      direction = "float",
-      size = 1.0,
-      float_opts = {
-        border = "curved",
-        title = "K9S",
-      },
-    })
+    k9s_terminal = Terminal.open("k9s")
   end
   k9s_terminal:toggle()
 end, { desc = "Toggle K9S CLI" })
