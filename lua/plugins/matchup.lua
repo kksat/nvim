658a1c00ce5480e954a8c2ecf1bd9eb84a92906a
Kirill Satarin <22549266+kksat@users.noreply.github.com>
2026-03-29 21:43:24 +0200

    change to init
    Signed-off-by: Kirill Satarin <22549266+kksat@users.noreply.github.com>

--- lua/plugins/matchup.lua
+++ lua/plugins/matchup.lua
@@ -1,6 +1,6 @@
 return {
     'andymass/vim-matchup',
-    setup = function()
+    init = function()
       -- may set any options here
       vim.g.matchup_matchparen_offscreen = { method = "popup" }
     end
