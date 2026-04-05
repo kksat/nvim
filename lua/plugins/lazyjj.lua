497f56026d523ee0e524dda87d1ff44ac5a0a9a1
Kirill Satarin <22549266+kksat@users.noreply.github.com>
2026-04-05 08:44:48 +0000

    refactor: modernize lazy plugin specs

--- lua/plugins/lazyjj.lua
+++ lua/plugins/lazyjj.lua
@@ -1,10 +1,7 @@
 return {
-  'swaits/lazyjj.nvim',
-  dependencies = 'nvim-lua/plenary.nvim',
-  opts = {},
-  setup = function()
-    require('lazyjj').setup({
-      mapping = '<leader>jj'
-    })
-  end,
+  "swaits/lazyjj.nvim",
+  dependencies = "nvim-lua/plenary.nvim",
+  opts = {
+    mapping = "<leader>jj",
+  },
 }
--- lua/plugins/matchup.lua
+++ lua/plugins/matchup.lua
@@ -1,7 +1,6 @@
 return {
-    'andymass/vim-matchup',
-    init = function()
-      -- may set any options here
-      vim.g.matchup_matchparen_offscreen = { method = "popup" }
-    end
-  }
+  "andymass/vim-matchup",
+  init = function()
+    vim.g.matchup_matchparen_offscreen = { method = "popup" }
+  end,
+}
