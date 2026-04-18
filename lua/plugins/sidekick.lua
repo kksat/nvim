80aaeb73b58259f7c93c8630c0570d7cd36d5ca2
Kirill Satarin <22549266+kksat@users.noreply.github.com>
2026-04-18 09:55:36 +0200

    remove custom key bind
    Signed-off-by: Kirill Satarin <22549266+kksat@users.noreply.github.com>

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -1,11 +1,3 @@
 return {
   "folke/sidekick.nvim",
-  keys = {
-    {
-      "<c-a>",
-      function() require("sidekick.cli").toggle() end,
-      desc = "Sidekick Toggle",
-      mode = { "n", "t", "i", "x" },
-    },
-  }
 }
